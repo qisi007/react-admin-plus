a91945ff7e7c416fe45408d1ea5d8e9b1f14002b
QJ刘贵生 <codeliuguisheng@163.com>
2020-09-22 17:07:06 +0800

    Updates

---
+++ precache-manifest.fc946027b8672be38f2b3fe7cf22eb2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb1a2154461b77fcabc0e6a235b9e8d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af358ac5c1f8b896d270",
+    "url": "./static/css/2.6c0293fb.chunk.css"
+  },
+  {
+    "revision": "7a3e09a35066c8f4d824",
+    "url": "./static/css/main.1d776d30.chunk.css"
+  },
+  {
+    "revision": "af358ac5c1f8b896d270",
+    "url": "./static/js/2.ea2078b9.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "./static/js/2.ea2078b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3e09a35066c8f4d824",
+    "url": "./static/js/main.601f64f1.chunk.js"
+  },
+  {
+    "revision": "435359b62f81b034e6c8",
+    "url": "./static/js/runtime-main.5aa34bfb.js"
+  }
+]);
