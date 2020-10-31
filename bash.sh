892a88ad94b94633f4b53ba5b81bd8aa49f60225
QJ刘贵生 <codeliuguisheng@163.com>
2020-10-31 18:35:12 +0800

    fix: menu

---
+++ bash.sh
@@ -0,0 +1,29 @@
+git pull origin master
+
+git merge master
+
+
+function obtain_git_branch {
+  br=`git branch | grep "*"`
+  echo ${br/* /}
+}
+branch=`obtain_git_branch`
+
+
+echo master代码合并完毕，正在提交本地分支代码...
+
+git add .
+
+git commit -m "fix: menu"
+
+echo 当前版本号: $branch
+
+git push --set-upstream origin $branch
+
+sleep 1s
+
+echo 代码已推送到远程分支
+
+npm run deploy
+
+echo 已构建完成
